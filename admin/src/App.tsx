a9da1eca529e5a4a33c55541da57ba5233e073e5
hieu.vo <hieu.vo@innogram.co>
2025-04-11 09:38:41 +0700

    Fix intergate

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
       <ToastContainer position="top-right" autoClose={3000} />
       <Routes>{renderRoutes(routes)}</Routes>
     </BrowserRouter>
--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/admin/', // Set the base path for the admin app
   server: {
     port: 3002,
   }
